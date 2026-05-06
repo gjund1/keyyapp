63580c59efd1f8c4480e326538606b2aaea770b8
gjund <gjund@free.fr>
2026-05-06 08:38:35 +0200

    update feature php

--- filter.php
+++ filter.php
@@ -3,7 +3,9 @@
 
 <!-- Filter Button -->
 <aside class="Main-filter Filter">
-    <div class="Filter-toggle"><span class="material-symbols-outlined font-arrow">arrow_right</span></div>
+    <div class="Filter-toggle">
+        <span class="material-symbols-outlined font-arrow">arrow_right</span>
+    </div>
     <div class="Filter-panel">
         <h3 class="Filter-panel-title">Affinez votre<br>Recherche</h3>
         <br>
--- js/list.js
+++ js/list.js
@@ -1,4 +1,39 @@
 // ------------------------------------ //
+//            Position GPS              //
+// ------------------------------------ //
+
+// Fonction pour récupérer la position GPS
+function getUserLocation(successCallback, errorCallback) {
+    if (!navigator.geolocation) {
+        errorCallback("Géolocalisation non supportée");
+        return;
+    };
+
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            successCallback(lat, lon);
+        },
+        function(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    errorCallback("Permission refusée");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    errorCallback("Position indisponible");
+                    break;
+                case error.TIMEOUT:
+                    errorCallback("Temps dépassé");
+                    break;
+                default:
+                    errorCallback("Erreur inconnue");
+            }
+        }
+    );
+};
+
+// ------------------------------------ //
 //               GPS                    //
 // ------------------------------------ //
 
